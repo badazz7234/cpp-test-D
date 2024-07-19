9b486b8af8194634517493d13a307eaa909327c8
badazz7234 <nathanunni@gmail.com>
2024-07-19 17:09:27 +0100

    INITIAL COMMIT NIGGAAAAAAA

---
+++ code/abusalim.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string boolean;
+
+    cout << "Are you BLACK? (YES or No)";
+    cin >> boolean;
+
+    if(boolean == "yes" || boolean == "YES" || boolean == "Yes" || boolean == "yEs" || boolean == "yeS" || boolean == "YEs" || boolean == "YeS" || boolean == "yES"){
+        cout << "GO TO JAIL NIGGA";
+    }
+
+    else if(boolean == "no" || boolean == "No" || boolean == "NO" || boolean == "nO"){
+        cout << "Enjoy your vacation gentleman :D ";
+    }
+
+    else{
+        cout << "ENTER A VALUE BITCH ASS NIGGA";
+    }
+    
+    /*cout << "Are you black? (Yes or No):  ";
+    ;*/
+    
+    
+    return 0;
+}
---
+++ code/black.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string boolean;
+
+    cout << "Are you BLACK? (YES or No)";
+    cin >> boolean;
+
+    if(boolean.tolower() == "yes"){
+        cout << "GO TO JAIL NIGGA";
+    }
+
+    else if(boolean.tolower() == "no"){
+        cout << "Enjoy your vacation gentleman :D ";
+    }
+
+    else{
+        cout << "ENTER A VALUE BITCH ASS NIGGA";
+    }
+    
+    /*cout << "Are you black? (Yes or No):  ";
+    ;*/
+    
+    
+    return 0;
+}
